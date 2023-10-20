784b9b1d9ad648cc3ba073a770d7933d1ec9921d
macgeeog <obilormacgeorge@gmail.com>
2023-10-19 17:43:03 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isdigit -  checks for digit file
+ * @c: parameter funtions to be checked
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+return (c >= '0' && c <= '9');
+}
